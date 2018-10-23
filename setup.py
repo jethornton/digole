d65b164e6d7a73c58eab4c12d60163a1e46b8584
jethornton <jthornton@gnipsel.com>
2018-10-23 05:30:52 -0500

    inital commit
    Signed-off-by: jethornton <jthornton@gnipsel.com>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='digole',
+      version='0.1',
+      description='Digole LCD Drivers',
+      url='https://github.com/jethornton/digole',
+      author='John Thornton',
+      author_email='bjt128@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
