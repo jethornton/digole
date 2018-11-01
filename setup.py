0cebddd631483c3207b1d01fcd7bd083fea21773
jethornton <jthornton@gnipsel.com>
2018-11-01 13:42:10 -0500

    Basic functions working now
    Signed-off-by: jethornton <jthornton@gnipsel.com>

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="digole",
-    version="0.0.1",
+    version="0.0.1.23",
     author="John Thornton",
     author_email="bjt128@gmail.com",
+    license="MIT License",
     description="Digole LCD Drivers",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jethornton/digole",
-    packages=setuptools.find_packages(),
+    packages=find_packages('src'),
+    package_dir={'':'src'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
     ],
+    py_modules={'digole'},
+    install_requires={'smbus2'},
 )
